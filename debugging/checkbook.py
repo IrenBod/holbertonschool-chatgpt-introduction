40d496adb0016706d74eaa2c17ff0fe6a1ec7e89
IrenBod <irinamora33@gmail.com>
2024-09-02 18:49:40 +0200

    add file checkbook

--- debugging/checkbook.py
+++ debugging/checkbook.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
+
 class Checkbook:
     def __init__(self):
         self.balance = 0.0
 
     def deposit(self, amount):
+        if amount <= 0:
+            print("Amount must be positive.")
+            return
         self.balance += amount
         print("Deposited ${:.2f}".format(amount))
         print("Current Balance: ${:.2f}".format(self.balance))
 
     def withdraw(self, amount):
+        if amount <= 0:
+            print("Amount must be positive.")
+            return
         if amount > self.balance:
             print("Insufficient funds to complete the withdrawal.")
         else:
